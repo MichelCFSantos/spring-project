c61e7f9e0cc9b9aff97941e11e2241297259b40d
Michel Wab <michelfsantos2020@gmail.com>
2025-08-14 20:19:56 -0300

    Commit

---
+++ src/main/java/com/example/springfirst/SpringFirstsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFirstsApplication.class, args);
+    }
+
+}
