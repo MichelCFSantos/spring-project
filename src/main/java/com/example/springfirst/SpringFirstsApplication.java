ed2f021177b2a365a3f6ecb04b65975108aeb8fc
Michel Wab <michelfsantos2020@gmail.com>
2025-08-17 18:47:13 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/springfirst/SpringFirstsApplication.java
+++ src/main/java/com/example/springfirst/SpringFirstsApplication.java
@@ -1,7 +1,9 @@
 package com.example.springfirst;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class SpringFirstsApplication {
