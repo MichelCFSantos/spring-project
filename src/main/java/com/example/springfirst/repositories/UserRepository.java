5c9962162acb0f24736856fb774f03b87ddbe342
Michel Wab <michelfsantos2020@gmail.com>
2025-08-25 19:48:00 -0300

    Camada de Serviços, registro de Componentes

--- src/main/java/com/example/springfirst/repositories/UserRepository.java
+++ src/main/java/com/example/springfirst/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.springfirst.repositories;
 
 import com.example.springfirst.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
