c8fa5b5be2cbb88c7c48994981f48dc7b6a3c420
Michel Wab <michelfsantos2020@gmail.com>
2025-08-27 21:22:03 -0300

    Order, Instant

--- src/main/java/com/example/springfirst/entities/User.java
+++ src/main/java/com/example/springfirst/entities/User.java
@@ -3,11 +3,13 @@ package com.example.springfirst.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
 @Entity
-@Table(name= "tb_user")
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -18,6 +20,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     public User() {
         super();
     }
@@ -70,6 +76,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -81,4 +91,6 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+
 }
